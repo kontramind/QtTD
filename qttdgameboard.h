91ae43b153c76c3b3626f8110c683f416a69dc98
Aleksandar Babic <aleksandar.babic@gmail.com>
2009-12-11 20:18:48 +0100

    fix compiling
    Signed-off-by: Aleksandar Babic <aleksandar.babic@gmail.com>

--- qttdgameboard.h
+++ qttdgameboard.h
@@ -2,11 +2,18 @@
 #define QTTDGAMEBOARD_H
 
 #include <QGraphicsScene>
+#include "qttd.h"
+#include "qttdcontext.h"
 
 class QtTDGameBoard : public QGraphicsScene
 {
 public:
     QtTDGameBoard();
+
+private:
+    QtTD game;
+    QtTDContext context;
+    qreal offsetX, offsetY;
 };
 
 #endif // QTTDGAMEBOARD_H
